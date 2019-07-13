f700c9cbc21517cd5713de3a75ede71754469664
ddotx <domemacintosh@gmail.com>
2019-07-13 11:25:29 +0700

    edited following component and router section

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  //*==>Template var pulled out locally to component
+  @ViewChild('sidenav') localSideNav;
+  toggle(){
+    this.localSideNav.toggle();
+  } //*<==
+  
   title = 'Angular Core Workshop';
+
+  links = [
+    {path:'/', icon:'home',title: 'Home'},
+    {path:'/customers', icon:'face', title: 'Customer'},
+    {path: '/projects', icon:'work', title:'Projects'}
+  ]
 }
